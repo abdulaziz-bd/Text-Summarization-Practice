76e30dfb3749cc955e375fcb28577698279504b5
abdulaziz-bd <aziz@uni-potsdam.de>
2023-10-05 13:19:01 +0200

    requirements added & installation done

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.1"
+
+REPO_NAME = "Text-Summarization-Practice"    
+AUTHOR_USER_NAME = "abdulaziz-bd"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "aziz@uni-potsdam.de"
+
+setuptools.setup(
+    name = SRC_REPO,
+    version =__version__,
+    author = AUTHOR_USER_NAME,
+    author_email = AUTHOR_EMAIL,
+    description = "A Python package for text summarization",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls = {
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir = {"": "src"}, 
+    packages = setuptools.find_packages(where="src"),
+)
+
